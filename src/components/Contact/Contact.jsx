9c1d39b1dc7dae26b03697a04f4083e84a56b91e
BalaVardhan Reddy <balavardhan@Codeyourself.org>
2024-10-01 10:03:03 +0530

    Portfolio

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:balavardhanreddy03@gmail.com">balavardhanreddy03@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/balavardhan-reddy-08b51b261/">linkedin.com/BalavardhanReddy</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Balavardhanreddy5872"> github.com/BalavardhanReddy</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Balavardhan </h1>
+        <p className={styles.description}>
+          I'm proficient in both front-end and back-end development, with experience in React and NodeJS. Feel free to reach out if you'd like to discuss further!
+        </p>
+
+        <a href="./resume.pdf" download className={styles.contactBtn}>
+          Download CV
+        </a>
+
+      </div>
+      <img
+        src={getImageUrl("hero/BaluPortfolio.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
